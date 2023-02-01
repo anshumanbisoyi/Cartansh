26d49c85293f060c6c5199307cb734cc4ffe576f
Anshuman Bisoyi <ansuman746@gmail.com>
2023-02-01 19:50:07 +0530

    products urls changed

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -20,8 +20,8 @@ const Products = ({ cat, filters, sort }) => {
       try {
         const res = await axios.get(
           cat
-            ? `http://localhost:3000/api/products?category=${cat}`
-            : "http://localhost:3000/api/products"
+            ? `https://cartansh-api.vercel.app/api/products?category=${cat}`
+            : "https://cartansh-api.vercel.app/api/products"
         );
         setProducts(res.data);
       } catch (err) {}
